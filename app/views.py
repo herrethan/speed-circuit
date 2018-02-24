765da201d49fb891d854c1c07bf2a8e6e531416e
Ethan Herr <eherr@reonomy.com>
2018-02-24 14:35:07 -0500

    setup

--- app/views.py
+++ app/views.py
@@ -9,10 +9,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/_users')
-def users():
-    users = [user.username for user in User.query.all()]
-    return jsonify(users)
+@app.route('/_status')
+def status():
+    status = {
+        'debug': app.config['DEBUG'],
+        'database': app.config['SQLALCHEMY_DATABASE_URI'],
+        # 'users': [user.username for user in User.query.all()],
+    }
+    return jsonify(status)
 
 
 @app.route('/about')
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+from livereload import Server, shell
 from app import app
 
+
 if __name__ == '__main__':
-    app.run()
+    # app.run(port=5000, use_reloader=True)
+
+    server = Server(app.wsgi_app)
+    server.watch('./app/static/dist/')
+    server.watch('./app/templates/')
+    server.serve(liveport=35729, port=5000, host='localhost')
