05fe3ae5c92f8f7683823a1c9263e54960b1ab22
Ethan Herr <eherr@reonomy.com>
2018-02-24 15:01:22 -0500

    test config

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/data.db'
+import os
+
+
+env = os.environ
+
+DEBUG = env.get('DEBUG', True)
+SQLALCHEMY_DATABASE_URI = env.get('SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/data.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = 8
